45ef8159bc76545b4272eebf66042bc151dd8172
agohorel <gohorelalex@gmail.com>
2020-02-12 14:47:45 -0500

    initial commit w/ middleware definitons

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,71 @@
-const express = require('express');
+const express = require("express");
+const db = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-  // do your magic!
+function validateUserId(id) {
+  return async function(req, res, next) {
+    try {
+      const user = await db.getById(id);
+      if (!user.length) {
+        res.status(400).json({ message: "invalid user id" });
+      } else {
+        req.user = user;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  next();
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const { body } = req;
+  if (!body) {
+    res.status(400).json({ message: "missing user data" });
+  } else if (!body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  }
+  next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const { body } = req;
+  if (!body) {
+    res.status(400).json({ message: "missing post data." });
+  } else if (!body.text) {
+    res.status(400).json({ message: "missing required text field." });
+  }
 }
 
 module.exports = router;
